abece2ed5fa7d2701e386ad99b12fa4dadbb2c29
Volodymyr Zahrebelnyi <vov41kttt@gmail.com>
2024-11-11 15:19:55 +0200

    fix filter

--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -1,7 +1,9 @@
-export const calculatePaginationData = ({ totalItems, page, perPage }) => {
+export const calculatePaginationData = (totalItems, page, perPage) => {
   const totalPages = Math.ceil(totalItems / perPage);
   const hasNextPage = page < totalPages;
   const hasPrevPage = page > 1;
+
+  console.log(totalPages);
 
   return {
     page,
