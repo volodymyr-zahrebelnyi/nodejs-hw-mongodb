1052fbe6566691de4c597fe08c12676171913364
Volodymyr Zahrebelnyi <vov41kttt@gmail.com>
2024-12-04 15:47:52 +0200

    small fix

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -2,7 +2,7 @@ import createHttpError from 'http-errors';
 import swaggerUI from 'swagger-ui-express';
 import fs from 'node:fs';
 
-import { SWAGGER_PATH } from '../constants';
+import { SWAGGER_PATH } from '../constants/index.js';
 
 export const swaggerDocs = () => {
   try {
